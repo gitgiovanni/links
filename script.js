5c14d033344b197ecc051d7060a7bacf48b27944
Giovanni Oséas <111138894+gitgiovanni@users.noreply.github.com>
2024-12-03 09:29:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const details = {
         1: {
-            title: "Detalhes do Link 1",
-            image: "https://fluig.andra.com.br/volume/stream/Rmx1aWc=/P3Q9MSZ2b2w9RGVmYXVsdCZpZD0xODAxJnZlcj0xMDAwJmZpbGU9Y3Vyc28tdGVjbmljby1kZS10aS5qcGcmY3JjPTEyMDE3MDU5OTkmc2l6ZT0wLjI1NTEyMSZ1SWQ9MjUmZlNJZD0xJnVTSWQ9MSZkPWZhbHNlJnRrbj0mcHVibGljVXJsPXRydWUmYXR0YWNoPWZhbHNl.jpg",
-            description: "Aqui estão os detalhes do Link 1.",
+            title: "Como saber qual é o seu email?",
+            image: "https://fluig.andra.com.br/volume/stream/Rmx1aWc=/P3Q9MSZ2b2w9RGVmYXVsdCZpZD0yMDMzJnZlcj0xMDAwJmZpbGU9MTgxLUFza2luZy1wb3dlcmZ1bC1xdWVzdGlvbnMtaW4teW91ci1vcmdhbmlzYXRpb25fbGFuZHNjYXBlLnBuZyZjcmM9MjIwMDE5OTQ1MCZzaXplPTAuMTA1OTIzJnVJZD0yNSZmU0lkPTEmdVNJZD0xJmQ9ZmFsc2UmdGtuPSZwdWJsaWNVcmw9dHJ1ZSZhdHRhY2g9ZmFsc2U=.png",
+            description: "Descobrir o seu email é uma tarefa simples! Clicando no link abaixo e digitando seu CPF, você consegue informações como seu email e a data de adimissão.",
             link: "#"
         },
         2: {
@@ -17,16 +17,16 @@ document.addEventListener("DOMContentLoaded", () => {
             link: "https://outlook.maiex13.com.br/owa/auth/logon.aspx?replaceCurrent=1&url=https%3a%2f%2foutlook.maiex13.com.br%2fowa"
         },
         3: {
-            title: "Detalhes do Link 3",
-            image: "https://fluig.andra.com.br/volume/stream/Rmx1aWc=/P3Q9MSZ2b2w9RGVmYXVsdCZpZD0xODAxJnZlcj0xMDAwJmZpbGU9Y3Vyc28tdGVjbmljby1kZS10aS5qcGcmY3JjPTEyMDE3MDU5OTkmc2l6ZT0wLjI1NTEyMSZ1SWQ9MjUmZlNJZD0xJnVTSWQ9MSZkPWZhbHNlJnRrbj0mcHVibGljVXJsPXRydWUmYXR0YWNoPWZhbHNl.jpg",
-            description: "Aqui estão os detalhes do Link 3.",
-            link: "#"
+            title: "Como acessar o fluig?",
+            image: "https://fluig.andra.com.br/volume/stream/Rmx1aWc=/P3Q9MSZ2b2w9RGVmYXVsdCZpZD0yMDM0JnZlcj0xMDAwJmZpbGU9Zmx1Z2FzK3BhZ2luYS5wbmcmY3JjPTc1MDU4OTg3NiZzaXplPTAuNzczNDA5JnVJZD0yNSZmU0lkPTEmdVNJZD0xJmQ9ZmFsc2UmdGtuPSZwdWJsaWNVcmw9dHJ1ZSZhdHRhY2g9ZmFsc2U=.png",
+            description: "Para acessar o fluig, você terá dois caminhos diferentes... um se você estiver usando o computador, e outro se você estiver usando o celular. PELO CELULAR: Primeiramente, baixe o aplicativo MY FLUIG, ele é tanto para android como pra IOS! Depois, clique em 'Esqueci minha senha' e insira seu Email. Vai chegar pra você um email simples, você clica e escolhe sua nova senha! Agora entre no fluig com email e a nova senha definida. PARA COMPUTADOR: O processo é parecido, você vai acessar o link abaixo, e então, clicar em esqueci minha senha e inserir o seu email. Vai chegar pra você um email simples onde você vai seguir as instruções e definir uma nova senha. Aí basta entrar no fluig com Email e a nova senha definida. Lembre-se: em ambos os casos é importante você salvar ou se lembrar da sua senha!    ",
+            link: "https://fluig.andra.com.br/portal/p/01/home"
         },
         4: {
-            title: "Detalhes do Link 4",
+            title: "Como acessar o Meu RH",
             image: "https://fluig.andra.com.br/volume/stream/Rmx1aWc=/P3Q9MSZ2b2w9RGVmYXVsdCZpZD0xODAxJnZlcj0xMDAwJmZpbGU9Y3Vyc28tdGVjbmljby1kZS10aS5qcGcmY3JjPTEyMDE3MDU5OTkmc2l6ZT0wLjI1NTEyMSZ1SWQ9MjUmZlNJZD0xJnVTSWQ9MSZkPWZhbHNlJnRrbj0mcHVibGljVXJsPXRydWUmYXR0YWNoPWZhbHNl.jpg",
-            description: "Aqui estão os detalhes do Link 4.",
-            link: "#"
+            description: "Acesse o link abaixo, lá tem todas as informações necessárias para o seu acesso ao Meu RH!",
+            link: "https://fluig.andra.com.br/portal/p/01/HoleriteAndra"
         }
     };
 
@@ -39,7 +39,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 <h2>${title}</h2>
                 <img src="${image}" alt="${title}" style="width: 100%; border-radius: 8px; margin-bottom: 20px;">
                 <p>${description}</p>
-                <a href="${link}" target="_blank" style="color: #0ea5e9; text-decoration: underline;">Acesse aqui</a>
+                <a href="${link}" target="_blank" style="color: #0ea5e9; text-decoration: underline;">Clique aqui</a>
             `;
             modal.classList.remove("hidden");
         });
